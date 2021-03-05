c40dcaa9e1470983bfe9c46f15b08184ea31d767
sami39 <hajjisami241@gmail.com>
2021-03-05 11:57:22 +0100

    showoneadherent

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Adherents;
 use App\Form\AdherentType;
+use Doctrine\ORM\Mapping\Id;
 use App\Repository\AdherentsRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
@@ -44,7 +46,7 @@ class AdminController extends AbstractController
     $form = $this->createForm(AdherentType::class, $projects);
     $form->handleRequest($request);
     $projets = $adherentsRepository->findAll();
-   
+
     if ($form->isSubmitted() && $form->isValid()) {
       $entityManager = $this->getDoctrine()->getManager();
       $entityManager->persist($projects);
@@ -97,5 +99,20 @@ class AdminController extends AbstractController
     $entityManager->remove($projects);
     $entityManager->flush();
     return $this->redirectToRoute('home');
+  }
+
+
+  
+  /**
+   * @Route("Afichepersone/{id} ", name="onepersonne")
+   */
+  public function Show(AdherentsRepository $AdherentsRepository,$id): Response
+  {
+    
+    $Adherents = $AdherentsRepository->findby(['id'=>$id]);
+
+    return $this->render('admin/unadherent.html.twig', [
+      'Adherents' => $Adherents
+    ]);
   }
 }
